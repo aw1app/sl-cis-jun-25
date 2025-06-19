2ab0868f9462255c5ff14268bb4eb627be6d9567
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-06-19 14:28:12 +0530

    css updates

--- JAVASCRIPT/Student.js
+++ JAVASCRIPT/Student.js
@@ -1,7 +1,8 @@
 class Student {
-    constructor(name, age, height) {
+    constructor(name, age, height, email) {
         this.name = name;
         this.age = age;
         this.height = height;
+        this.email=email;
     }
 };
