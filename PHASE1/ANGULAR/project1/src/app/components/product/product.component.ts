5142df60d65761ccc858d54bb55f6750086d0cfa
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-06-20 11:56:31 +0530

    angular updates

---
+++ PHASE1/ANGULAR/project1/src/app/components/product/product.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  name:string="iPhone";
+  price:number=15000;
+
+}
