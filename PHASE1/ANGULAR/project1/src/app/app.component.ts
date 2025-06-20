1d4bc2415c83b52a39de6cd1ab65e18686e8d0de
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-06-20 14:35:24 +0530

    angular updates

--- PHASE1/ANGULAR/project1/src/app/app.component.ts
+++ PHASE1/ANGULAR/project1/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductComponent } from './components/product/product.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,13 @@ export class AppComponent {
   title = 'ABC XYZ';
 
   x=1000;
+
+ products:Product[] = [
+    new Product(1, "iPhone", 75000, "Great Phone 1", true, 'images/111.png'),
+    new Product(2, "Nokia", 5000, "Simple Phone 2", true, 'images/222.png'),
+    new Product(3, "Samsung", 15000, "Good Phone 3", true, 'images/333.png'),
+    new Product(4, "Motorala", 25000, "Nice Phone 4", false, 'images/444.png')
+  ];
 
   name1:string="Dell Laptop";
   price1:number=555;
--- PHASE1/ANGULAR/project1/src/app/components/product-list/product-list.component.ts
+++ PHASE1/ANGULAR/project1/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
 
@@ -11,15 +11,8 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductListComponent {
 
-  products!: Product[];
+ @Input("products") products!: Product[];
 
-  constructor() {
-    this.products = [
-      new Product(1, "iPhone", 75000, "Great Phone", true, 'images/111.png'),
-      new Product(2, "Nokia", 5000, "Simple Phone", true, 'images/222.png'),
-      new Product(3, "Samsung", 15000, "Good Phone", true, 'images/333.png'),
-      new Product(4, "Motorala", 25000, "Nice Phone", false, 'images/444.png')
-    ];
-  }
+
 
 }
