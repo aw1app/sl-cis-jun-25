bf39dbe39bef5d95fd5ad8438874f97f25dfedc9
aw1app <aw1app@outlook.com>
2025-07-16 12:14:33 +0530

    core java

---
+++ PHASE2/HelloWorld/src/DataTypesAndOperatorsDemo.java
@@ -0,0 +1,30 @@
+
+public class DataTypesAndOperatorsDemo {
+
+	public static void main(String[] args) {
+
+		int a = 10; // assignment operator '=' used here
+		int b = -7;
+		
+		boolean c = false;
+		boolean d = true;
+		
+		float x=10.95f;
+		double y=10565656575757.9556765555d;
+		
+		char c1 = 'A';
+		char c2 = 'B';
+		
+		// arithmetic operation on a  and b
+		int r1= a + b;
+		
+		System.out.println("r1 = " + r1);
+		
+		// logical and operation on c and d.
+		boolean f = c && d ;
+		System.out.println("f = " + f);
+		
+
+	}
+
+}
