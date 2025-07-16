d38db6707b54c0fb198932d1c60ddb034138ba0f
aw1app <aw1app@outlook.com>
2025-07-16 14:12:23 +0530

    core java arrays

--- PHASE2/DataHandling/src/ArraysDemo.java
+++ PHASE2/DataHandling/src/ArraysDemo.java
@@ -5,10 +5,16 @@ public class ArraysDemo {
 
 		// Declare and initialize an integer array with 10 elements
 		int[] myArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
-		
+
 		// Access and print the second element of the array
 		System.out.println("Access and print the second element of the array");
 		System.out.println(myArray[1]);
+
+		// 
+		System.out.println("Looping through the full array:");
+		for (int i = 0; i < myArray.length; i++) {
+			System.out.print(myArray[i] + "\t");
+		}
 
 	}
 
