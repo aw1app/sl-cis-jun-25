b21efb2e7f2d5987673931dbb86b5175cedd3495
aw1app <aw1app@outlook.com>
2025-07-17 10:28:17 +0530

    core java oops

--- PHASE2/Airline/src/Pilot.java
+++ PHASE2/Airline/src/Pilot.java
@@ -9,7 +9,7 @@ public class Pilot {
 
 	// behaviours
 	boolean fly(Plane plane) {
-
+		return false;
 	}
 
 	String display() {
--- PHASE2/Airline/src/Plane.java
+++ PHASE2/Airline/src/Plane.java
@@ -1,4 +1,47 @@
 
 public class Plane {
+	
+	private String name;
+	private String planeID;
+	
+	private int capacity;
+	private String model;
+	
+	boolean isInternational = false;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public boolean isInternational() {
+		return isInternational;
+	}
+
+	public void setInternational(boolean isInternational) {
+		this.isInternational = isInternational;
+	}
+
+	boolean takeOff(){
+		
+		return false;
+	}
+	
+	boolean land(){
+		return false;
+	}
 
 }
