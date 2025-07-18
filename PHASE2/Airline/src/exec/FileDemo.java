8a55139f667993eada35c48a8c7e1d94821af1a7
aw1app <aw1app@outlook.com>
2025-07-18 15:01:52 +0530

    core java multithreading chefsdemo with synchronized

--- PHASE2/Airline/src/exec/FileDemo.java
+++ PHASE2/Airline/src/exec/FileDemo.java
@@ -7,15 +7,20 @@ import java.nio.file.Path;
 public class FileDemo {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		Path helloPath = Path.of("hello.txt");
-		
-		Path pathTemp = Files.createFile(helloPath);
-		
+
+		Path pathTemp = null;		
+		if (Files.exists(helloPath) == false)
+			pathTemp = Files.createFile(helloPath);
+
 		boolean fileExist = Files.exists(helloPath);
-		
-		System.out.println(fileExist);
+
+		// Print full path of the file represented by helloPath Path.
+		System.out.println(helloPath.toFile().getAbsolutePath() + " exists = " + fileExist);
 
 	}
+	
+
 
 }
