1439f4ec89c80f2791114ba297457da445601f8b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-01 10:39:15 +0530

    Java servlets

---
+++ PHASE2/ServletProject1/src/main/java/com/sl/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.sl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Hello World and Hello India");
+		
+		out.close();
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
