acbb8b461c7c9686a582b3ee8ebda4a3cbb3ca7b
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-05 12:12:35 +0530

    Session Tracking Hidden Form feilds demo

---
+++ PHASE2/SessionTrackingUsingHiddenFormFeilds/src/main/java/com/sl/ServletA.java
@@ -0,0 +1,44 @@
+package com.sl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletA")
+public class ServletA extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Random random = new Random();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int sessionId = random.nextInt();
+
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		// Response from Servlet A
+		out.println(" <h1> Welcome to Amazon.com </h1>");
+		out.println(" <h2> This is Servlet A web page </h2>");
+		
+		out.println("<html><form action='ServletB' method='POST'>");
+		out.println("<input type='hidden' name='sessionId' value='" + sessionId + "'>");
+		out.println("<input type='submit' value='submit' >");
+		out.println("</form>");
+		out.println("<script>document.forms[0].submit();</script></html>");
+
+		String contextURL = request.getServletContext().getContextPath();		
+		
+
+		out.close();
+
+	}
+
+}
