e909bd0e12fad20551c5dbc3afaf823302f478b9
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-08-05 14:40:46 +0530

    Session Tracking using HttpSession object

--- PHASE2/SessionTrackingUsingHttpSession/src/main/java/com/sl/ServletA.java
+++ PHASE2/SessionTrackingUsingHttpSession/src/main/java/com/sl/ServletA.java
@@ -1,43 +1,36 @@
 package com.sl;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Random;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 
+// Session Tracking using HttpSession
 
 @WebServlet("/ServletA")
 public class ServletA extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	Random random = new Random();
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		int sessionId = random.nextInt();
-		
-		Cookie cookie = new Cookie("sessionId",""+sessionId);
-		
 		response.setContentType("text/html");
-
 		PrintWriter out = response.getWriter();
+
+		HttpSession session = request.getSession(true); // true will ensure if a session is not present, then create a
+														// new session.
 
 		// Response from Servlet A
 		out.println(" <h1> Welcome to Amazon.com </h1>");
 		out.println(" <h2> This is Servlet A web page </h2>");
-		
-		String contextURL = request.getServletContext().getContextPath();
-		
-		response.addCookie(cookie);		
-		
+
+		out.println(" <p> Just now created a brand new http Session or retrieved an existing one </p>");
+		out.println(" <p> Session Id is  </p>" + session.getId());
+
 		out.close();
 
 	}
--- PHASE2/SessionTrackingUsingHttpSession/src/main/java/com/sl/ServletB.java
+++ PHASE2/SessionTrackingUsingHttpSession/src/main/java/com/sl/ServletB.java
@@ -2,42 +2,35 @@ package com.sl;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
+// Session Tracking using HttpSession
 
-
-@WebServlet("/ServletA")
-public class ServletA extends HttpServlet {
+@WebServlet("/ServletB")
+public class ServletB extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	Random random = new Random();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		int sessionId = random.nextInt();
-		
-		Cookie cookie = new Cookie("sessionId",""+sessionId);
-		
+
 		response.setContentType("text/html");
 
 		PrintWriter out = response.getWriter();
 
-		// Response from Servlet A
+		HttpSession session = request.getSession(false); // false will ensure if a session is not present, then it will
+															// return null and not create a new session
+
+		// Response from Servlet B
 		out.println(" <h1> Welcome to Amazon.com </h1>");
-		out.println(" <h2> This is Servlet A web page </h2>");
-		
-		String contextURL = request.getServletContext().getContextPath();
-		
-		response.addCookie(cookie);		
-		
+		out.println(" <h2> This is Servlet B web page </h2>");
+		out.println(" <h3> Here is your session ID : " + (session!=null ? session.getId() : null )+ "</h3>");
+
 		out.close();
 
 	}
