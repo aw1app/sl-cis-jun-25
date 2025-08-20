880dcbe1ad9357bf4265b298036fd78da567d327
aw1app <aw1app@outlook.com>
2025-08-20 12:08:32 +0530

    junit life cycle methods demo

--- PHASE3/junitdemo/src/test/java/junitdemo/LifeCycleMethodsDemo.java
+++ PHASE3/junitdemo/src/test/java/junitdemo/LifeCycleMethodsDemo.java
@@ -1,17 +1,36 @@
 package junitdemo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LifeCycleMethodsDemo {
 	Calculator cal;
 	
+	@BeforeAll
+	static void myBeforeAll() {
+		System.out.println(" INSIDE myBeforeAll. This method will run before starting the test cases in this class  ");
+	}
+	
+	@AfterAll
+	static void myAfterAll() {
+		System.out.println(" INSIDE myAfterAll. This method will run after all test cases completed in this class  ");
+	}
+	
 	@BeforeEach
 	void myBeforeEach() {
-		System.out.println("INSIDE myBeforeEach");
+		System.out.println("\t INSIDE myBeforeEach");
 		 cal = new Calculator();
+	}
+	
+	@AfterEach
+	void myAfterEach() {
+		System.out.println("\t INSIDE myAfterEach \n");
+		 cal = null;
 	}
 	
 	@Test
