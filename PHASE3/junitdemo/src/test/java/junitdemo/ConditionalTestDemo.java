16d0670b9383b57d37ef6073f3ca6da3b12af6b8
aw1app <aw1app@outlook.com>
2025-08-20 12:23:11 +0530

    junit conditional tess demo

--- PHASE3/junitdemo/src/test/java/junitdemo/ConditionalTestDemo.java
+++ PHASE3/junitdemo/src/test/java/junitdemo/ConditionalTestDemo.java
@@ -3,7 +3,9 @@ package junitdemo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.condition.OS;
 
 class ConditionalTestDemo {
@@ -23,10 +25,24 @@ class ConditionalTestDemo {
 	}
 	
 	@Test
-	@EnabledOnOs(value =  {OS.LINUX} )
-	public void testAddOnLinux() {
+	@EnabledOnOs(value =  {OS.MAC} )
+	public void testAddOnMac() {
 
-		System.out.println("INSIDE testAddOnLinux");
+		System.out.println("INSIDE testAddOnMac");
+
+		Calculator cal = new Calculator();
+
+		int x = 2;
+		int y = 5;
+
+		assertEquals(7, cal.add(x, y));
+	}
+	
+	@Test
+	@EnabledForJreRange(min =  JRE.JAVA_11, max = JRE.JAVA_21 )
+	public void testAddOnJRERange() {
+
+		System.out.println("INSIDE testAddOnJRERange");
 
 		Calculator cal = new Calculator();
 
