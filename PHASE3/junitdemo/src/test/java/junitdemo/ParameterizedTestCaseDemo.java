d1813ba20fdd038297637404c6f0c42011946cd1
aw1app <aw1app@outlook.com>
2025-08-20 13:57:04 +0530

    junit parametrized test demo

--- PHASE3/junitdemo/src/test/java/junitdemo/ParameterizedTestCaseDemo.java
+++ PHASE3/junitdemo/src/test/java/junitdemo/ParameterizedTestCaseDemo.java
@@ -14,7 +14,9 @@ class ParameterizedTestCaseDemo {
 				"1, 1, 2",
 				"-2, 3, 1",
 				"10, -5, 5",
-				"0,0,0"
+				"0,0,0",
+				"1000000000,1000000000,2000000000",
+				/*"5000000000,5000000000,10000000000" this set of values fail the test */
 			}
 			)
 	void testAdd(int input1, int input2, int expectedResult) {
