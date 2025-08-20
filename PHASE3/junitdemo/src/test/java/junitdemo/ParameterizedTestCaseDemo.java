3c0fb190774c6ff0b67b4662119aacd620989c99
aw1app <aw1app@outlook.com>
2025-08-20 13:49:49 +0530

    junit parametrized test demo

---
+++ PHASE3/junitdemo/src/test/java/junitdemo/ParameterizedTestCaseDemo.java
@@ -0,0 +1,30 @@
+package junitdemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class ParameterizedTestCaseDemo {
+
+	@ParameterizedTest
+	@CsvSource(
+			{ 
+				"1, 1, 2",
+				"-2, 3, 1",
+				"10, -5, 5",
+				"0,0,0"
+			}
+			)
+	void testAdd(int input1, int input2, int expectedResult) {
+		Calculator cal = new Calculator();
+
+	
+		int actualResult = cal.add(input1, input2);
+
+		assertEquals(expectedResult, actualResult);
+
+	}
+
+}
