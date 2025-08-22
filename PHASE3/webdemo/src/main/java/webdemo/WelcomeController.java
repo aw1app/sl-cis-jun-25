de4962a2c8bb2a9a538d07d9f7a53d2cb8a6c583
aw1app <aw1app@outlook.com>
2025-08-22 10:16:25 +0530

    spring web mvc demo with controller

---
+++ PHASE3/webdemo/src/main/java/webdemo/WelcomeController.java
@@ -0,0 +1,25 @@
+package webdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class WelcomeController {
+	
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		
+		return "ab";	// go to ab.jsp	
+	}
+	
+	
+	@GetMapping("/welcome1")
+	@ResponseBody
+	public String welcome1() {
+		
+		return "a";		
+	}
+
+}
