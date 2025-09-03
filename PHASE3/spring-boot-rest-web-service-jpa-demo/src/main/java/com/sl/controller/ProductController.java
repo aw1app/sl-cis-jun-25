dbd36c89308adc4533c1789c4734a9cb9c0084d5
ashokwork1 <54761512+ashokwork1@users.noreply.github.com>
2025-09-03 14:51:43 +0530

    spring boot rest server jpa for products table in mysql

---
+++ PHASE3/spring-boot-rest-web-service-jpa-demo/src/main/java/com/sl/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.sl.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sl.entity.Product;
+import com.sl.repositry.ProductRepositry;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	@Autowired
+	ProductRepositry productRepositry;
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Product>> getAllProducts(){
+		List<Product> products = productRepositry.findAll();
+		
+		return new ResponseEntity(products, HttpStatusCode.valueOf(200));
+	}
+	
+
+}
