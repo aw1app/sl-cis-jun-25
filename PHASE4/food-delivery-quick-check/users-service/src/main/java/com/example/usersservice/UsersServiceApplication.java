ca506400ae494b549bb1103717ff766940491569
aw1app <aw1app@outlook.com>
2025-10-15 12:52:34 +0530

    AWS code build demo and dummy capstone project

--- PHASE4/food-delivery-quick-check/users-service/src/main/java/com/example/usersservice/UsersServiceApplication.java
+++ PHASE4/food-delivery-quick-check/users-service/src/main/java/com/example/usersservice/UsersServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.foodsservice;
+package com.example.usersservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
